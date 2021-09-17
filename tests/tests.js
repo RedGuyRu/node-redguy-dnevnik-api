0611cc22b23e0d85a352f1cb955d70db9f1542a4
Ilya <admin@redguy.ru>
2021-09-17 21:06:19 +0300

    New Messages system

--- tests/tests.js
+++
@@ -1,5 +0,0 @@
-const dnevnikApi = require("../lib");
-let api = new dnevnikApi(process.env.TOKEN);
-api.Social().Users().getbysocialid('vk',195680093).then((token) => {
-    console.log(token);
-});
